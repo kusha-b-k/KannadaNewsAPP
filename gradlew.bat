d8f77b9bd6e1f9f608b6c71c9bdf56efddfc1653
KUSHA B K <kusha.b.k@gmail.com>
2016-05-12 22:03:26 +0530

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -22,7 +22,7 @@ if defined JAVA_HOME goto findJavaFromJavaHome
 set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
 if "%ERRORLEVEL%" == "0" goto init
-if "%OS%"=="Windows_NT" setlocal
+
 echo.
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
 echo.
